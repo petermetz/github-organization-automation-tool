e6d5afc80d13d8dd58f84409d1c13568b6714160
Peter Somogyvari <peter.metz@unarin.com>
2021-09-30 20:21:56 -0700

    chore(constants): bump version
    Signed-off-by: Peter Somogyvari <peter.metz@unarin.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const PROJECT_NAME_SHORT = "GOAT";
 export const PROJECT_NAME_LONG = "Github Organization Automation Tool";
 export const NPM_PACKAGE_NAME = "github-organization-automation-tool";
-export const NPM_PACKAGE_VERSION = "1.1.0-rc.7";
+export const NPM_PACKAGE_VERSION = "1.1.0-rc.8";
